7024ce8fa76f08792fca8b5287d031128e96c7a7
Dmitriy.Klochkov <dmitriy.klochkov@x5.ru>
2020-10-06 13:39:20 +0400

    Создал коллекции

---
+++ ArrayColllection.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class ArrayColllection<E> implements TypeCollection {
+
+    public static void main(String[] args) {
+        ArrayList<String> ArrayList = new ArrayList<>();
+        ArrayList.add("a");
+        ArrayList.add("4");
+        ArrayList.add("5");
+    }
+
+    @Override
+    public int size() {
+        return this.size();
+    }
+
+    @Override
+    public void add(Object item) {
+    }
+
+    @Override
+    public void remove(int index) {
+    }
+
+    @Override
+    public void remove(Object item) {
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+    }
+}
+
